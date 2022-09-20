6ce87b648f53b72e359a1054962175293285f85b
António Santos <antoniopocsantos@gmail.com>
2022-09-20 20:05:49 +0100

    Inovar
    Inovar

---
+++ Inovar/CreateFromInovarAlunos.ps1
@@ -0,0 +1,14 @@
+﻿$dominio = "yourdomain.com"
+
+#Create users from INOVAR Alunos
+
+Import-CSV  C:\eb\alunos.csv  -delimiter ";"  -Header Nome,Processo | Foreach-Object{
+   Write-Host $_.Nome
+   $mail = $_.Processo + $dominio
+   #Write-Host $_.Processo $dominio -Separator ''
+   Write-Host $mail
+   New-MsolUser -UserPrincipalName $mail -DisplayName $_.Nome -Password "MySuperPassword"
+   Set-MsolUser -UserPrincipalName $mail -UsageLocation PT
+   Set-MsolUserLicense -UserPrincipalName $mail -AddLicenses "myOrg:STANDARDWOFFPACK_STUDENT"
+   #Example Licence: myOrg:STANDARDWOFFPACK_STUDENT
+}
