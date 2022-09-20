1c7e8e1a69de2fa17c7bb02fe6e3aa0e7b591351
António Santos <antoniopocsantos@gmail.com>
2022-09-20 20:08:35 +0100

    Update CreateFromInovarAlunos.ps1

--- Inovar/CreateFromInovarAlunos.ps1
+++ Inovar/CreateFromInovarAlunos.ps1
@@ -5,7 +5,6 @@
 Import-CSV  C:\eb\alunos.csv  -delimiter ";"  -Header Nome,Processo | Foreach-Object{
    Write-Host $_.Nome
    $mail = $_.Processo + $dominio
-   #Write-Host $_.Processo $dominio -Separator ''
    Write-Host $mail
    New-MsolUser -UserPrincipalName $mail -DisplayName $_.Nome -Password "MySuperPassword"
    Set-MsolUser -UserPrincipalName $mail -UsageLocation PT
